195b0908917c8345700c0ab2e53ebc3e71974abb
Piene Luijpers <71266227+pieneluijpers@users.noreply.github.com>
2020-09-28 09:37:22 +0200

    Add files via upload

---
+++ js/sketch.js
@@ -0,0 +1,171 @@
+function setup() {
+  createCanvas(800,800);
+  background(40);
+  // put setup code here
+
+   
+//4e blok//
+  noStroke()
+  fill(100,149,237)
+  rect(400,50,300,800)
+
+  fill(255)
+  rect(425,620,250,30)
+
+  fill(255)
+  rect(425,680,250,30)
+
+  fill(255)
+  rect(425,740,250,30)
+  //4e blok//
+
+//3e blok
+noStroke()
+  fill(90,129,237)
+  rect(250,600,300,200) 
+
+  fill(90,129,220)
+  rect(400,50,150,600)
+  
+  fill(255)
+  rect(425,90,250,30)
+
+  fill(255)
+  rect(425,140,250,30)
+
+  fill(255)
+  rect(425,200,250,30)
+
+  fill(255)
+  rect(425,260,250,30)
+
+  fill(255)
+  rect(425,320,250,30)
+
+  fill(255)
+  rect(425,380,250,30)
+
+  fill(255)
+  rect(425,440,250,30)
+
+  fill(255)
+  rect(425,500,250,30)
+
+  fill(255)
+  rect(425,560,250,30)
+
+stroke(9000)
+  fill(0,0,230)
+  ellipse(680,520,150,150)
+  noStroke()
+
+
+
+
+  //3e blok//
+
+//2e blok
+  fill(90,119,237)
+  rect(580,500,200,300)
+
+fill(255)
+  rect(350,600,200,20)
+
+  fill(255)
+  rect(350,650,170,20)
+
+  fill(255)
+  rect(350,690,170,20)
+
+  fill(255)
+  rect(350,730,170,20)
+
+  fill(255)
+  rect(350,770,170,20)
+
+  
+  
+  //2e blok//
+
+//1e blok
+fill(255)
+rect(250,260,80,50)
+
+  fill(90,109,237)
+  rect(60,300,290,500)
+
+  fill(255)
+  rect(90,330,230,20)
+
+  fill(255)
+  rect(40,380,330,40)
+
+  fill(255)
+  rect(40,440,330,40)
+
+  fill(255)
+rect(70,760,260,50)
+
+
+//1e blok
+
+//blokjes4x3// //1e rij//
+  fill(255)
+  rect(90,520,45,55)
+
+  fill(255)
+  rect(150,520,45,55)
+
+  fill(255)
+  rect(210,520,45,55)
+
+  fill(255)
+  rect(270,520,45,55)
+
+  //blokjes4x3// //2e rij//
+  fill(255)
+  rect(90,600,45,55)
+
+  fill(255)
+  rect(150,600,45,55)
+
+  fill(255)
+  rect(210,600,45,55)
+
+  fill(255)
+  rect(270,600,45,55)
+
+   //blokjes4x3// //3e rij//
+   fill(255)
+   rect(90,680,45,55)
+ 
+   fill(255)
+   rect(150,680,45,55)
+ 
+   fill(255)
+   rect(210,680,45,55)
+ 
+   fill(255)
+   rect(270,680,45,55)
+
+
+
+
+
+  //1e blok//
+
+  fill(255)
+  rect(730,530,30,250)
+
+  rect(690,530,30,250)
+
+  rect(650,530,30,250)
+
+  rect(610,530,30,250)
+
+  rect(580,500,200,20)
+}
+
+function draw() {
+  // put drawing code here
+}
